36b163a573487af2c13c531f7df1680aa489bcd0
Saurabh Parmar <parmarsaurabh87@gmail.com>
2017-07-09 16:38:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/saurabh/firebasecameraimg/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.saurabh.firebasecameraimg;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mSelectImhBtn;
+    private ImageView mImageView;
+    public static final int CAMERA_REQUEST_CODE = 1;
+    private StorageReference mStorage;
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        mSelectImhBtn = (Button) findViewById(R.id.selectImgBtn);
+        mImageView = (ImageView) findViewById(R.id.imageView);
+
+        mProgressDialog = new ProgressDialog(this);
+
+        mSelectImhBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                startActivityForResult(intent, CAMERA_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+
+            mProgressDialog.setMessage("File Uploading...");
+            mProgressDialog.show();
+
+            Uri uri = data.getData();
+
+            StorageReference filePath = mStorage.child("Photos").child(uri.getLastPathSegment());
+
+            filePath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    mProgressDialog.dismiss();
+
+                    @SuppressWarnings("VisibleForTests") Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                    Picasso.with(MainActivity.this).load(downloadUrl).centerCrop().into(mImageView);
+
+                    Toast.makeText(MainActivity.this, "Uploading Finish", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
